c326669078e8ec28dc47bbe3fc06c4a8c5632d95
Volodymyr <semkivvolodya@gmail.com>
2025-07-08 18:03:51 +0200

    feat: logging progress function

--- PROJECT/etl_practice.py
+++ PROJECT/etl_practice.py
@@ -1,93 +1,82 @@
-import glob
+# Code for ETL operations on Country-GDP data
+# Importing the required libraries
+from bs4 import BeautifulSoup
+import requests
 import pandas as pd
-import xml.etree.ElementTree as ET
-from datetime import datetime
-
-log_file = 'log_file.txt'
-target_file = 'transformed_data.csv'
-
-def extract_from_csv(file_to_process):
-    dataframe = pd.read_csv(file_to_process)
-    return dataframe
-
-def extract_from_json(file_to_process):
-    dataframe = pd.read_json(file_to_process, lines=True)
-    return dataframe
-
-def get_xml_headers(file_to_process):
-    tree = ET.parse(file_to_process)
-    root = tree.getroot()
-    first = root[0]
-    headers = [child.tag for child in first]
-    return headers
-
-headers = get_xml_headers('source\\used_car_prices1.xml')
-print(headers)
-
-def extract_from_xml(file_to_process):
-    dataframe = pd.DataFrame(columns=["car_model","year_of_manufacture", "price", "fuel" ])
-    tree = ET.parse(file_to_process)
-    root = tree.getroot()
-    for car in root: 
-        model = car.find("car_model").text
-        year = car.find("year_of_manufacture").text
-        price = car.find("price").text
-        fuel = car.find("fuel").text
-        record = {
-            "car_model": model,
-            "year_of_manufacture": year,
-            "price": price,
-            "fuel": fuel
-        }
-        dataframe = pd.concat([dataframe, pd.DataFrame([record])], ignore_index=True)
-    return dataframe
-
-def extract():
-    dataframe = pd.DataFrame(columns=["car_model","year_of_manufacture", "price", "fuel" ])
-
-    #process all csv files, except the target file
-    for csvfile in glob.glob("source\\*.csv"):
-        if csvfile != target_file:
-            dataframe = pd.concat([dataframe, extract_from_csv(csvfile)], ignore_index=True)
-
-    #process all json files, except the target file
-    for jsonfile in glob.glob("source\\*.json"):
-        if jsonfile != target_file:
-            dataframe = pd.concat([dataframe, extract_from_json(jsonfile)], ignore_index=True)
-
-    #process all xml files, except the target file
-    for xmlfile in glob.glob("source\\*.xml"):
-        dataframe = pd.concat([dataframe, extract_from_xml(xmlfile)], ignore_index=True)
-    return dataframe
-
-def transform(data): 
-    data["price"] = pd.to_numeric(data["price"], errors='coerce').round(2)
-    return data
-    
-def load_data(target_file, data):
-    data.to_csv(target_file, index=False)
-
+import numpy as np
+import sqlite3
+from datetime import datetime 
+def extract(url, table_attribs):
+    ''' The purpose of this function is to extract the required
+    information from the website and save it to a dataframe. The
+    function returns the dataframe for further processing. '''
+    page = requests.get(url).text
+    data = BeautifulSoup(page,'html.parser')
+    df = pd.DataFrame(columns=table_attribs)
+    tables = data.find_all('tbody')
+    rows = tables[2].find_all('tr')
+    for row in rows:
+        col = row.find_all('td')
+        if len(col)!=0:
+            if col[0].find('a') is not None and '—' not in col[2]:
+                data_dict = {"Country": col[0].a.contents[0],
+                             "GDP_USD_millions": col[2].contents[0]}
+                df1 = pd.DataFrame(data_dict, index=[0])
+                df = pd.concat([df,df1], ignore_index=True)
+    return df
+def transform(df):
+    ''' This function converts the GDP information from Currency
+    format to float value, transforms the information of GDP from
+    USD (Millions) to USD (Billions) rounding to 2 decimal places.
+    The function returns the transformed dataframe.'''
+    GDP_list = df["GDP_USD_millions"].tolist()
+    GDP_list = [float("".join(x.split(','))) for x in GDP_list]
+    GDP_list = [np.round(x/1000,2) for x in GDP_list]
+    df["GDP_USD_millions"] = GDP_list
+    df=df.rename(columns = {"GDP_USD_millions":"GDP_USD_billions"})
+    return df
+def load_to_csv(df, csv_path):
+    ''' This function saves the final dataframe as a `CSV` file 
+    in the provided path. Function returns nothing.'''
+    df.to_csv(csv_path)
+def load_to_db(df, sql_connection, table_name):
+    ''' This function saves the final dataframe to as a database table
+    with the provided name. Function returns nothing.'''
+    df.to_sql(table_name, sql_connection, if_exists='replace', index=False)
+def run_query(query_statement, sql_connection):
+    ''' This function runs the stated query on the database table and
+    prints the output on the terminal. Function returns nothing. '''
+    print(query_statement)
+    query_output = pd.read_sql(query_statement, sql_connection)
+    print(query_output)
 def log_progress(message):
-    timestamp_format = "%Y-%m-%d %H:%M:%S"
-    now = datetime.now()
-    timestamp = now.strftime(timestamp_format)
-    with open(log_file, "a") as f:
-        f.write(f"{timestamp}, {message}\n")
-
-if __name__ == "__main__":
-    log_progress("ETL process started.")
-
-    log_progress("Extraction phase started.")
-    extracted_data = extract()
-    log_progress(f"Extraction phase completed. Extracted {len(extracted_data)} records.")
-
-    log_progress("Transformation phase started.")
-    transformed_data = transform(extracted_data)
-    
-    log_progress("Transformation phase completed.")
-
-    log_progress("Loading phase started.")
-    load_data(target_file, transformed_data)
-    log_progress(f"Loading phase completed. Data saved to {target_file}.")
-
-    log_progress("ETL process completed.")
+    ''' This function logs the mentioned message at a given stage of the 
+    code execution to a log file. Function returns nothing.'''
+    timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second 
+    now = datetime.now() # get current timestamp 
+    timestamp = now.strftime(timestamp_format) 
+    with open("./etl_project_log.txt","a") as f: 
+        f.write(timestamp + ' : ' + message + '\n')    
+''' Here, you define the required entities and call the relevant 
+functions in the correct order to complete the project. Note that this
+portion is not inside any function.'''
+url = 'https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
+table_attribs = ["Country", "GDP_USD_millions"]
+db_name = 'World_Economies.db'
+table_name = 'Countries_by_GDP'
+csv_path = './Countries_by_GDP.csv'
+log_progress('Preliminaries complete. Initiating ETL process')
+df = extract(url, table_attribs)
+log_progress('Data extraction complete. Initiating Transformation process')
+df = transform(df)
+log_progress('Data transformation complete. Initiating loading process')
+load_to_csv(df, csv_path)
+log_progress('Data saved to CSV file')
+sql_connection = sqlite3.connect('World_Economies.db')
+log_progress('SQL Connection initiated.')
+load_to_db(df, sql_connection, table_name)
+log_progress('Data loaded to Database as table. Running the query')
+query_statement = f"SELECT * from {table_name} WHERE GDP_USD_billions >= 100"
+run_query(query_statement, sql_connection)
+log_progress('Process Complete.')
+sql_connection.close()
